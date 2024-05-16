46ed28bad070920b24b784502711886d8ec1971f
202116302 <wls5258@naver.com>
2024-05-16 16:20:07 +0900

    연구실 환경 현황

---
+++ lab_state.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import pixoo_reset
+
+def main():
+    pixoo_reset.reset_display()
+
+
+    dashboard_url = 'http://web01.taegon.kr:7600/test'
+    response = requests.get(dashboard_url)
+    result = response.text
+    result = json.loads(result)
+    temp = result['lab']['day_temp'][-1]
+    humid = result['lab']['day_humid'][-1]
+    time = result['lab']['day_time'][-1]
+    date = result['lab']['date'][-1]
+    date_str = f"{date.split('-')[1]}/{date.split('-')[2]}"
+
+
+    pixoo_url = 'http://web01.taegon.kr:5000/text'
+
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    data = {
+        # 'text': '온실 온습도',
+        # 'text': f'{time}',
+        # 'text': f'{temp}℃',
+        'text': f'{temp}℃  \n{humid}%\n        {date_str}\n{time}\n 연구실',
+        'x': '3',
+        'y': '0',
+        'push_immediately': 'true'
+
+    }
+
+    response = requests.post(pixoo_url, headers=headers, data=data)
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+if __name__ == '__main__':
+    main()
